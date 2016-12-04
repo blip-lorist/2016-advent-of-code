7068ba9799004264544c137b23aada82493a6da3
Loraine <loraine@lorainekv.com>
2016-12-04 13:19:50 -0800

    Test for possible triangle.

--- day_03/test_day_03.rb
+++ day_03/test_day_03.rb
@@ -1,7 +1,10 @@
 require "minitest/autorun"
+require "./day_03.rb"
 
-describe "foo" do
-  it "foo" do
+describe "determines if a triangle is possible" do
+  it "returns a true or false decision" do
+    decision = possible_triangle?([5, 10, 25])
+    assert_equal(decision, false)
   end
 end
 
