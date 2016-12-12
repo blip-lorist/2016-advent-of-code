0a5a90e7f69d4be55fcba8ed6b171d9f225a9115
Loraine <loraine@lorainekv.com>
2016-12-11 22:31:44 -0800

    All passing tests.

--- day_04/test_day_04.rb
+++ day_04/test_day_04.rb
@@ -11,6 +11,21 @@ describe "identifies a real room" do
     sector_id = real_room_sector_id("aaaaa-bbb-z-y-x-123[abxyz]")
     assert_equal(sector_id, 123)
   end
+  
+  it "returns sector id for real room with all ties" do
+    sector_id = real_room_sector_id("a-b-c-d-e-f-g-h-987[abcde]")
+    assert_equal(sector_id, 987)
+  end
+
+  it "returns sector id for real room unordered letters" do
+    sector_id = real_room_sector_id("not-a-real-room-404[oarel]")
+    assert_equal(sector_id, 404)
+  end
+
+  it "returns 0 for unreal room " do
+    sector_id = real_room_sector_id("totally-real-room-200[decoy]")
+    assert_equal(sector_id, 0)
+  end
 
 end
 
