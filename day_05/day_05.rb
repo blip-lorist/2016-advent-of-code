e26c5439c5aaf1e42c1475177afb497ee2deff36
Loraine <loraine@lorainekv.com>
2016-12-24 14:42:41 -0800

    Very inefficient password finder. Refactor opp - don't process the same element position more than once.

--- day_05/day_05.rb
+++ day_05/day_05.rb
@@ -1,4 +1,10 @@
 require 'digest/md5'
+class String
+  def is_i?
+    # http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby
+    /\A[-+]?\d+\z/ === self 
+  end
+end
 
 def md5_encrypt(string)
   Digest::MD5.hexdigest(string)
@@ -14,7 +20,26 @@ def get_password_character(string)
   end
 end
 
-def build_password(door_id, int=1, password=[])
+def get_password_position_and_character(string)
+  encrypted_string = md5_encrypt(string)
+  position_and_character = []
+
+  if encrypted_string.start_with?("00000")
+
+    position = encrypted_string[5]
+    if position.is_i? && position.to_i.between?(0,7)
+      position = position.to_i
+      character = encrypted_string[6]
+      position_and_character << position 
+      position_and_character << character
+      return position_and_character
+    end
+  else
+    return nil
+  end
+end
+
+def build_password(door_id, int=0, password=[])
   # Base case: when password length is 8
   until password.length == 8
     unencrypted_string = door_id + int.to_s
@@ -28,5 +53,28 @@ def build_password(door_id, int=1, password=[])
   return password.join("")
 end
 
-puts build_password("abbhdwsy")
+def build_password_with_positions(door_id, int=0)
+  password = Array.new(8, nil)
 
+  # Locked elements - horribly inefficient. Shouldn't be processing the same element more than once. 
+  locked_elements = []
+  # Base case: when password array no longer contains nil
+  while password.include?(nil) 
+    unencrypted_string = door_id + int.to_s
+    next_position_and_character = get_password_position_and_character(unencrypted_string)
+    if next_position_and_character
+      position, character = next_position_and_character
+
+      unless locked_elements.include?(position)
+        password[position] = character 
+        locked_elements << position
+      end
+    end
+    int += 1
+  end
+  
+  return password.join("")
+end
+
+#puts build_password("abbhdwsy")
+puts build_password_with_positions("abbhdwsy")
--- day_05/test_day_05.rb
+++ day_05/test_day_05.rb
@@ -16,10 +16,24 @@ describe "get_password_character" do
   end
 end
 
- describe "build_password" do 
-   it "returns an eight-character password" do
-     door_id = "abc"
-     password = build_password(door_id)
-     assert_equal(password, "18f47a30")
-   end
- end
+describe "get_password_position_and_character" do 
+  it "returns an array containing the sixth and seventh characters of an encrypted string" do
+    unencrypted_string = "abc3231929"
+    assert_equal(get_password_position_and_character(unencrypted_string), [1, "5"])
+  end
+end
+
+describe "build password with positions" do 
+  it "assembles a password with positional information" do 
+    door_id = "abc" 
+    password = build_password_with_positions(door_id)
+    assert_equal(password, "05ace8e3")
+  end
+end
+# describe "build_password" do 
+#    it "returns an eight-character password" do
+#      door_id = "abc"
+#      password = build_password(door_id)
+#      assert_equal(password, "18f47a30")
+#    end
+#  end
